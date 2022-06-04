694ce2e114beefbae3316db0d72754d555ce1fb6
Dylan Harper <dylanharper4@icloud.com>
2022-06-04 12:39:36 -0700

    Init flask and basic db config

--- app.py
+++ app.py
@@ -4,3 +4,13 @@ from flask_marshmallow import Marshmallow
 import statistics
 import json
 import os
+
+#initialize app
+app = Flask(__name__)
+
+#initialize db
+base_directory = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(base_directory, 'db.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+ma = Marshmallow(app)
